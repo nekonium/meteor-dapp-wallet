7bb7f097701fd0e50d454f92c830cdf1e1667129
nyatla <nyatla39@gmail.com>
2017-06-22 21:00:12 +0900

    Nukonium ブランドに変更

--- app/client/templates/elements/selectableUnit.js
+++ app/client/templates/elements/selectableUnit.js
@@ -17,8 +17,8 @@ The available units
 @property selectableUnits
 */
 selectableUnits = [{
-    text: 'ETHER',
-    value: 'ether'
+    text: 'NEKONIUM',
+    value: 'nekonium'
 },
 {
     text: 'FINNEY', //(µΞ)
@@ -45,7 +45,7 @@ if (moment().format('MM-DD')=='04-01') {
         { text: 'LOVELACE', value: 'lovelace'},
         { text: 'BABBAGE', value: 'babbage'},
         { text: 'WEI', value: 'wei'},
-        { text: 'NOETHER', value: 'noether'})
+        { text: 'NONUKO', value: 'nonuko'})
 
 // Claude's Birthday
 } else if (moment().format('MM-DD')=='04-30') { 
