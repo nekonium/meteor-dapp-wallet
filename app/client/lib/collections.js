4f2eb1d7c8e300190ac218369aa3506817ef61e0
nyatla <nyatla39@gmail.com>
2017-06-25 10:08:54 +0900

    mango2patch trial

--- app/client/lib/collections.js
+++ app/client/lib/collections.js
@@ -4,25 +4,25 @@
 
 
 Wallets = new Mongo.Collection('wallets', {connection: null});
-new PersistentMinimongo2(Wallets, 'ethereum_wallet');
+new PersistentMinimongo(Wallets, 'ethereum_wallet');
 
 CustomContracts = new Mongo.Collection('custom-contracts', {connection: null});
-new PersistentMinimongo2(CustomContracts, 'ethereum_wallet');
+new PersistentMinimongo(CustomContracts, 'ethereum_wallet');
 
 // Contains the transactions
 Transactions = new Mongo.Collection('transactions', {connection: null});
-new PersistentMinimongo2(Transactions, 'ethereum_wallet');
+new PersistentMinimongo(Transactions, 'ethereum_wallet');
 
 // Contains the pending confirmations
 PendingConfirmations = new Mongo.Collection('pending-confirmations', {connection: null});
-new PersistentMinimongo2(PendingConfirmations, 'ethereum_wallet');
+new PersistentMinimongo(PendingConfirmations, 'ethereum_wallet');
 
 // Contains the custom contract events
 Events = new Mongo.Collection('events', {connection: null});
-new PersistentMinimongo2(Events, 'ethereum_wallet');
+new PersistentMinimongo(Events, 'ethereum_wallet');
 
 // Contains Coin Information
 Tokens = new Mongo.Collection('tokens', {connection: null});
-new PersistentMinimongo2(Tokens, 'ethereum_wallet');
+new PersistentMinimongo(Tokens, 'ethereum_wallet');
 
 
