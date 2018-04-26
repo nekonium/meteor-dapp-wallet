0604111f805e875e9375d8316e43e460732e3146
nyatla <nyatla39@gmail.com>
2018-04-26 18:41:29 +0900

    web3インスタンスの生成方式変更

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -4,6 +4,9 @@ if(typeof nekonium === 'undefined'){
   nekonium={};
 }
 if(typeof nekonium.web3 === 'undefined'){
-  console.log("add nekonium web3");
+  console.log("add nekonium web3 by http");
   nekonium.web3 = new nekonium.Web3(new nekonium.Web3.providers.HttpProvider("http://localhost:8293"));
+}else{
+  console.log("add nekonium web3 by current probider.");
+  nekonium.web3 = new nekonium.Web3(nekonium.web3.currentProvider);
 }
